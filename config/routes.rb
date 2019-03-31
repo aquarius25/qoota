99fea56357fe15375b4107595c03ebdeea36f6d3
aqua25 <h.aqua25@gmail.com>
2019-04-01 02:25:49 +0900

    Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+        registrations: 'users/registrations'
+  }
+  root "groups#index"
   resources :users
 end
