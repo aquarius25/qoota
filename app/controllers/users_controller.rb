d2839de7768e753caac1d611938b9cef6ef9c2b7
aqua25 <h.aqua25@gmail.com>
2019-04-12 13:35:26 +0900

    Delete pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.page(params[:page]).per(10).order("created_at DESC")
   end
 end
