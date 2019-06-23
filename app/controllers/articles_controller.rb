b6b46305371512fdd5e5217d8318a39e5c745417
aqua25 <h.aqua25@gmail.com>
2019-06-23 18:36:57 +0900

    tag_list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,8 @@ class ArticlesController < ApplicationController
   def index
     @search = Article.search(params[:q])
     @articles = @search.result.includes(:user).page(params[:page]).per(10).order("created_at DESC")
-    @tag_articles = Article.all.includes(:user).order("created_at DESC")
     if params[:tag_name]
-      @tag_article = @tag_articles.tagged_with("#{params[:tag_name]}")
+      @articles = @articles.tagged_with("#{params[:tag_name]}")
     end
   end
 
