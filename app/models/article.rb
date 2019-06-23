093bb5985b8cb95c2d3200fbdde6b4c0ae47325c
aqua25 <h.aqua25@gmail.com>
2019-06-23 17:12:18 +0900

    tag

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,6 @@ class Article < ApplicationRecord
   belongs_to :user
 
   # gem acts-as-taggable-on
-  acts_as_taggable_on :interests
+  acts_as_taggable_on :interests # label_list が追加される
+  acts_as_taggable            # acts_as_taggable_on :tags のエイリアス
 end
