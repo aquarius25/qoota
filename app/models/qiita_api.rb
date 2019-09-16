8b5eed0f26cf2d4a3a7280dacf5cd64bc3ed42dc
aqua25 <h.aqua25@gmail.com>
2019-09-16 20:16:28 +0900

    qiita items search function

---
+++ app/models/qiita_api.rb
@@ -0,0 +1,20 @@
+require "json"
+
+class QiitaApi
+  def get_items(params=nil)
+    url = 'https://qiita.com'
+    connection = Faraday.new(url: url)
+    query = if params
+              'tag:' + params[:tag] + ' updated:>' + params[:update_at] + ' stocks:>' + params[:stocks]
+            else
+              'tag:Rails updated:>2019-02 stocks:>3'
+            end
+    response = connection.get do |req|
+      req.url '/api/v2/items', page: 1, per_page: 100, query: query
+    end
+    json = JSON.parse(response.body)
+    json.sort_by { |i| i['likes_count']}.reverse.map do |item|
+      { title: item['title'], url: item['url'], likes_count: item['likes_count'] }
+    end.take(20)
+  end
+end
